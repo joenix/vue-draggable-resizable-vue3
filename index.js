fa70e13dfb50bae199b2da382adaf9f7a23cb5db
Joenix <joenix@qq.com>
2021-10-31 15:17:12 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// Use Component
+import VueDraggableResizable from "./src/index.vue";
+
+// Set Install for `app.use`
+function install(app) {
+  // If is Installed
+  if (install.installed) {
+    return;
+  }
+
+  // is Install
+  install.installed = true;
+
+  // Register Component name `VueDraggableResizable`
+  app.component("VueDraggableResizable", VueDraggableResizable);
+}
+
+// Export as Plugin
+export const plugin = { install };
+
+/**
+ * Register Component in Vue2 for Async
+ * ========== ========== ==========
+ */
+
+// Set Global
+let Global = null;
+
+// in Window
+if (typeof window !== "undefined") {
+  Global = window;
+}
+
+// in Global
+if (typeof global !== "undefined") {
+  Global = global;
+}
+
+// Set GlobalVue
+let GlobalVue = Global.App || Global.Vue;
+
+// do Register
+if (GlobalVue && GlobalVue.use) {
+  // Register Plugin
+  GlobalVue.use(plugin);
+}
+
+// Export Component
+export default VueDraggableResizable;
---
+++ readme.md
@@ -0,0 +1,42 @@
+# vue-draggable-resizable-vue3
+
+> use vue-draggable-resizable in vue3
+
+- [Original Repo](https://github.com/mauricius/vue-draggable-resizable)
+- [Document](https://www.npmjs.com/package/vue-draggable-resizable)
+- [Live Playboard](https://mauricius.github.io/vue-draggable-resizable)
+
+## install
+
+```sh
+npm install vue-draggable-resizable-vue3
+# or
+yarn add vue-draggable-resizable-vue3
+```
+
+## use
+
+```js
+import { plugin } from "vue-draggable-resizable-vue3";
+
+app.use(plugin);
+```
+
+## or
+
+```js
+import VueDraggableResizable from "vue-draggable-resizable-vue3";
+
+export default {
+  components: {
+    VueDraggableResizable
+  }
+};
+```
+
+## tip
+
+```sh
+# use less
+yarn add -D less less-loader
+```
